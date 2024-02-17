d695c6c39c34d256a076c8fc6873cb99de5a051f
saikirananumalla <saikirananumalla@gmail.com>
2024-02-16 19:57:55 -0500

    handle not found error for job and company

--- resolvers.js
+++ resolvers.js
@@ -1,11 +1,26 @@
 import {getJob, getJobs, getJobsByCompany} from "./db/jobs.js";
 import {getCompany} from "./db/companies.js";
+import {GraphQLError} from 'graphql';
 
 export const resolvers = {
     Query : {
         jobs: () => getJobs(),
-        job: (_root, {id}) => getJob(id),
-        company: (_root, {id}) => getCompany(id),
+
+        job: async (_root, {id}) => {
+            const job = await getJob(id);
+            if (!job) {
+                throw notFoundError('No Job found with id ' + id)
+            }
+            return job;
+        },
+
+        company: async (_root, {id}) => {
+            const company = await getCompany(id);
+            if (!company) {
+                throw notFoundError('No Company found with id ' + id);
+            }
+            return company;
+        },
     },
 
     Company: {
@@ -17,6 +32,12 @@ export const resolvers = {
         company: (job) => getCompany(job.companyId)
     }
 };
+
+function notFoundError(message) {
+    return new GraphQLError(message, {
+        extensions: {code: 'NOT_FOUND'},
+    });
+}
 
 function toIsoDate(value) {
     return value.slice(0, 'yyyy-mm-dd'.length);
